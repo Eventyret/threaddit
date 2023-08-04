1dac11cf4f7f6ad85505bf4fe305b6ebf88643c0
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-04 15:44:51 +0100

    Added assets

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,4 +1,14 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
 function Topbar() {
-  return <h1>Top Bar</h1>
+  return (
+    <nav className='topbar'>
+      <Link href="/" className='flex items-center gap-4'>
+        <Image src='/logo.svg' width={28} height={28} alt="Threaddit Logo" />
+        <p className='text-heading3-bold text-light-1 max-xs:hidden'>Threaddit</p>
+      </Link>
+    </nav>
+  )
 }
 export default Topbar
