e5807fb9169756921f1c011e8632c7fb211e1e5d
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-04 16:27:18 +0100

    Created left sidebar completed topbar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -6,7 +6,7 @@ function Topbar() {
   return (
     <nav className='topbar'>
       <Link href="/" className='flex items-center gap-4'>
-        <Image src='/logo.svg' width={28} height={28} alt="Threaddit Logo" />
+        <Image src='/assets/logo.svg' width={28} height={28} alt="Threaddit Logo" />
         <p className='text-heading3-bold text-light-1 max-xs:hidden'>Threaddit</p>
       </Link>
       <div className='flex items-center gap-1'>
@@ -14,7 +14,7 @@ function Topbar() {
           <SignedIn>
             <SignOutButton>
               <div className='flex cursor-pointer'>
-                <Image src='/logout.svg' width={24} height={24} alt="Logout" />
+                <Image src='/assets/logout.svg' width={24} height={24} alt="Logout" />
               </div>
             </SignOutButton>
           </SignedIn>
