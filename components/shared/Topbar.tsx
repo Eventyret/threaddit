f8c50259f8096aaf591f1e7e0989482b5f6b14a9
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-04 16:04:20 +0100

    Finished Topbar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,6 @@
-import Link from 'next/link'
+import { OrganizationSwitcher, SignOutButton, SignedIn } from '@clerk/nextjs'
 import Image from 'next/image'
+import Link from 'next/link'
 
 function Topbar() {
   return (
@@ -8,6 +9,23 @@ function Topbar() {
         <Image src='/logo.svg' width={28} height={28} alt="Threaddit Logo" />
         <p className='text-heading3-bold text-light-1 max-xs:hidden'>Threaddit</p>
       </Link>
+      <div className='flex items-center gap-1'>
+        <div className='block md:hidden'>
+          <SignedIn>
+            <SignOutButton>
+              <div className='flex cursor-pointer'>
+                <Image src='/logout.svg' width={24} height={24} alt="Logout" />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+
+        </div>
+        <OrganizationSwitcher appearance={{
+          elements: {
+            organizationSwitcherTrigger: "py-2 px-4"
+          }
+        }} />
+      </div>
     </nav>
   )
 }
