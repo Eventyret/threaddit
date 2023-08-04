519ea9f7e598e158a9c818c796be42b3e044cd21
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-04 16:53:49 +0100

    Added right sidebar

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -2,7 +2,10 @@ function RightSidebar() {
   return (
     <section className='custom-scrollbar rightsidebar'>
       <div className='flex flex-col justify-start flex-1'>
-        <h3>Suggested Communities</h3>
+        <h3 className='text-heading4-medium text-light-1'>Suggested Communities</h3>
+      </div>
+      <div className='flex flex-col justify-start flex-1'>
+        <h3 className='text-heading4-medium text-light-1'>Suggested Developers</h3>
       </div>
     </section>
   )
