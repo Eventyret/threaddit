a2650b4f3a6a09ffa1bb2abe9d43ca6b76e4b696
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-04 16:50:36 +0100

    Created Right sidebar for communities

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,4 +1,10 @@
 function RightSidebar() {
-  return <h1>Right Sidebar</h1>
+  return (
+    <section className='custom-scrollbar rightsidebar'>
+      <div className='flex flex-col justify-start flex-1'>
+        <h3>Suggested Communities</h3>
+      </div>
+    </section>
+  )
 }
 export default RightSidebar
