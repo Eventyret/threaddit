1c7e1fc02067cd1e0a2e65db69d2ad446d842e15
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-06 10:20:57 +0100

    Working on threads and posts

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,26 +1,37 @@
-import AccountProfile from '@/components/forms/AccountProfile'
-import { currentUser } from "@clerk/nextjs"
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-async function Page() {
-  const user = await currentUser()
-  const userInfo = {}
+import { fetchUser } from "@/lib/actions/user.actions";
+import AccountProfile from "@/components/forms/AccountProfile";
+
+async function OnboardingPage() {
+  const user = await currentUser();
+  if (!user) return null; // to avoid typescript warnings
+
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
 
   const userData = {
-    id: user?.id,
-    objectId: user?._id,
-    username: user?.username || user?.username,
-    name: userInfo?.name || user?.firstName || "",
-    bio: userInfo?.bio || "",
-    image: userInfo?.image || user?.imageUrl || "",
-  }
+    id: user.id,
+    objectId: userInfo?._id,
+    username: userInfo ? userInfo?.username : user.username,
+    name: userInfo ? userInfo?.name : user.firstName ?? "",
+    bio: userInfo ? userInfo?.bio : "",
+    image: userInfo ? userInfo?.image : user.imageUrl,
+  };
+
   return (
     <main className='flex flex-col justify-start max-w-3xl px-10 py-20 mx-auto'>
-      <h1 className='head-text'> Onboarding</h1>
-      <p className='mt- text-base-regular text-light-2'></p>
+      <h1 className='head-text'>Onboarding</h1>
+      <p className='mt-3 text-base-regular text-light-2'>
+        Complete your profile now, to use Threds.
+      </p>
+
       <section className='p-10 mt-9 bg-dark-2'>
-        <AccountProfile user={userData} btnTitle="Continue" />
+        <AccountProfile user={userData} btnTitle='Continue' />
       </section>
     </main>
-  )
+  );
 }
-export default Page
+
+export default OnboardingPage;
