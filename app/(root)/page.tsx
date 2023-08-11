3721fbf7cc76c78c39077d1159c8a736356ab560
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-11 11:50:08 +0100

    Finished root page will continue with thread card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,34 @@
 import { fetchPosts } from '@/lib/actions/thread.actions';
+import User from '@/lib/models/user.model';
+import { currentUser } from '@clerk/nextjs';
 
 export default async function Home() {
-  const result = await fetchPosts(1, 30);
-  console.log(result)
+  const results = await fetchPosts(1, 30);
+  const user = await currentUser();
   return (
     <>
       <h1 className='text-left head-text'>Home</h1>
+      <section className='flex flex-col gap-10 mt-9'>
+        {results.posts.length === 0 ? (
+          <p className='no-result'>No threads found</p>
+        ) : (
+          <>
+            {results.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user?.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section >
     </>
   )
 }
