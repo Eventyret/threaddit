dad48cc8c41ee29a1d5d7fb8fd5d11d722b43862
Simen Daehlin <simen.daehlin@strapi.io>
2023-08-11 13:38:19 +0100

    WIP Thread Card

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+import Link from 'next/link'
 
 interface Props {
   id: string
@@ -26,8 +28,41 @@ const ThreadCard = ({ id,
   createdAt,
   comments }: Props) => {
   return (
-    <article>
-      <h2 className='text-small-regular text-light-2'>{content}</h2>
+    <article className='flex flex-col w-full rounded-xl bg-dark-2 p-7'>
+      <div className='flex items-start justify-between'>
+        <div className='flex flex-row flex-1 w-full gap-4'>
+          <div className='flex flex-col items-center'>
+            <Link
+              href={`/profile/${author.id}`}
+              className='relative h-11 w-11'
+            >
+              <Image src={author.image} alt='Profile Image' fill className='rounded-full cursor-pointer' />
+            </Link>
+            <div className='thread-card_bar' />
+          </div>
+          <div className='flex flex-col w-full'>
+            <Link
+              href={`/profile/${author.id}`}
+              className='w-fit'
+            >
+              <h4 className='cursor-pointer text-base-semibold text-light-1'>{author.name}</h4>
+            </Link>
+            <p className='mt-2 text-small-regular text-light-2'>{content}</p>
+            <div className='flex flex-col gap-3 mt-5'>
+              <div className='flex gap-3.5'>
+                <Image src="/assets/heart-gray.svg" alt='Heart' width={24} height={24} className='object-contain cursor-pointer' />
+                <Link
+                  href={`/thread/${id}`}>
+                  <Image src="/assets/reply.svg" alt='Reply' width={24} height={24} className='object-contain cursor-pointer' />
+                </Link>
+                <Image src="/assets/repost.svg" alt='Repost' width={24} height={24} className='object-contain cursor-pointer' />
+                <Image src="/assets/share.svg" alt='Share' width={24} height={24} className='object-contain cursor-pointer' />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </article>
   )
 }
